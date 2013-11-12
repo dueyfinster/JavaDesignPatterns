9b4bef9c6df909b05f48d522c579295e56466edf
Neil Grogan <neil@grogan.ie>
2013-11-12 17:08:02 +0000

    Updated comments for builder

--- src/main/java/creational/builder/BuilderRunner.java
+++ src/main/java/creational/builder/BuilderRunner.java
@@ -2,11 +2,11 @@ package creational.builder;
 
 public class BuilderRunner {
 	public static void main(String[] args) {
-		HouseBuilder hb = new HouseBuilder();
-		hb.setHouseType(HouseType.DETACHED);
-		hb.setNoOfDoors(3);
-		hb.setNoOfWindows(10);
-		House house = hb.getResult();
+		HouseBuilder hb = new HouseBuilder(); // create a new builder
+		hb.setHouseType(HouseType.DETACHED); // set attribute on builder
+		hb.setNoOfDoors(3); // set attribute on builder
+		hb.setNoOfWindows(10); // set attribute on builder
+		House house = hb.getResult(); // only now do we create a house when all the pieces are in place
 		System.out.println(house);
 	}
 
