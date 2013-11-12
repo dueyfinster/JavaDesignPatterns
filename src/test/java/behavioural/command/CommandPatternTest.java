d37185f520d30b7406f0685f56dffced48f64ee3
Neil Grogan <neil@grogan.ie>
2013-11-12 18:48:40 +0000

    Added CI and failing test case

---
+++ src/test/java/behavioural/command/CommandPatternTest.java
@@ -0,0 +1,14 @@
+package behavioural.command;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CommandPatternTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
