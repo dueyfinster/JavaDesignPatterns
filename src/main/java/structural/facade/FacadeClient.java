8d8f28c7068001eecca21185fc020b33343997c6
Neil Grogan <neil@grogan.ie>
2013-11-12 14:41:17 +0000

    Updated Facade

--- src/main/java/structural/facade/FacadeClient.java
+++ src/main/java/structural/facade/FacadeClient.java
@@ -3,7 +3,7 @@ package structural.facade;
 public class FacadeClient {
 
 	public static void main(String[] args) {
-		ComputerFacade cf = new ComputerFacade();
+		ComputerFacade cf = new ComputerFacade(); // Hide low-level details with facade
 		cf.start();
 	}
 
