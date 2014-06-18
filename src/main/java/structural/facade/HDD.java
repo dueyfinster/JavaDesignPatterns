ec1304837893687cb49a995cc015d57b44f3424a
Neil Grogan <neil@grogan.ie>
2014-06-18 21:10:57 +0100

    Updates to fix build

--- src/main/java/structural/facade/HDD.java
+++ src/main/java/structural/facade/HDD.java
@@ -1,7 +1,7 @@
 package structural.facade;
 
 public class HDD {
-	private static final byte ZERO_LENGTH_ARRAY[] = new byte[0];
+    private static final byte[] ZERO_LENGTH_ARRAY = new byte[0];
 
     public byte[] read(final long block, final int size) {
         System.out.println("HDD is loading from block: " + block + " with size " + size);
