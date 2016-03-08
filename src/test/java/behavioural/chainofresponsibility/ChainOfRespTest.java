652447cee6e0482ee67f0ef597f29b71f4dab0d6
Neil Grogan <neil@ngrogan.com>
2016-03-08 14:24:30 +0000

    Added Chain of Resp test

--- src/test/java/behavioural/chainofresponsibility/ChainOfRespTest.java
+++ src/test/java/behavioural/chainofresponsibility/ChainOfRespTest.java
@@ -13,6 +13,9 @@ import org.junit.Test;
 public class ChainOfRespTest {
         private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+        private final String INFO_CONTENT = "Standard Console::Logger: This is an information.\n";
+        private final String DEBUG_CONTENT = "Error Console::Logger: This is an debug level information.\nFile::Logger: This is an debug level information.\nStandard Console::Logger: This is an debug level information.\n";
+        private final String ERROR_CONTENT = "Error Console::Logger: This is an error information.\nStandard Console::Logger: This is an error information.\n";
 
         @Before
         public void setUp() throws Exception {
@@ -20,9 +23,43 @@ public class ChainOfRespTest {
             System.setErr(new PrintStream(errContent));
         }
         
+        
+        
         @Test
-        @Ignore
-        public void testFirstCallGetsProxy(){
+        public void test_info_logs_correct_levels(){
+            AbstractLogger loggerChain = getChainOfLoggers();
+            loggerChain.logMessage(LogLevels.INFO, "This is an information.");
+            final String outMessage = outContent.toString();
+            assertTrue(outMessage.equals(INFO_CONTENT));
         }
+        
+        @Test
+        public void test_debug_logs_correct_levels(){
+            AbstractLogger loggerChain = getChainOfLoggers();
+            loggerChain.logMessage(LogLevels.DEBUG, "This is an debug level information.");
+            final String outMessage = outContent.toString();
+            assertTrue(outMessage.equals(DEBUG_CONTENT));
+        }
+        
+        @Test
+        public void test_error_logs_correct_levels(){
+            AbstractLogger loggerChain = getChainOfLoggers();
+            loggerChain.logMessage(LogLevels.ERROR, "This is an error information.");
+            final String outMessage = outContent.toString();
+            assertTrue(outMessage.equals(ERROR_CONTENT));
+        }
+        
+        
+        private AbstractLogger getChainOfLoggers(){
+
+            AbstractLogger errorLogger = new ErrorLogger(LogLevels.ERROR);
+            AbstractLogger fileLogger = new FileLogger(LogLevels.DEBUG);
+            AbstractLogger consoleLogger = new ConsoleLogger(LogLevels.INFO);
+
+            errorLogger.setNextLogger(fileLogger);
+            fileLogger.setNextLogger(consoleLogger);
+
+            return errorLogger;
+         }
 
 }
