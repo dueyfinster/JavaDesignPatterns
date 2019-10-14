bedd01feacce3704fdc5146913bbdd3280210e8b
Neil Grogan <neil.grogan@ericsson.com>
2019-10-14 19:07:08 +0100

    Updated Composite Test

--- src/main/java/structural/composite/Employee.java
+++ src/main/java/structural/composite/Employee.java
@@ -9,37 +9,37 @@ public class Employee {
     private final double salary;
     private List<Employee> subordinates;
 
-    Employee(final String name, final String dept, final double salary){
+    Employee(final String name, final String dept, final double salary) {
         this.name = name;
         this.dept = dept;
         this.salary = salary;
         subordinates = new ArrayList<Employee>();
     }
 
-    public void addSubordinate(final Employee e){
+    public void addSubordinate(final Employee e) {
         subordinates.add(e);
     }
 
-    public void removeSubordinate(final Employee e){
+    public void removeSubordinate(final Employee e) {
         subordinates.remove(e);
     }
 
-    public List<Employee> getSubordinates(){
+    public List<Employee> getSubordinates() {
         return subordinates;
     }
-    
-    public void printSubordinates(){
+
+    public void printSubordinates() {
         System.out.println(this);
         for (Employee headEmployee : this.getSubordinates()) {
-            System.out.println(headEmployee);
-            
+            System.out.println("\t" + headEmployee);
+
             for (Employee employee : headEmployee.getSubordinates()) {
-               System.out.println(employee);
+                System.out.println("\t\t" + employee);
             }
-         } 
+        }
     }
 
-    public String toString(){
-        return "Employee [name: "+name+", dept: "+dept+", salary: "+salary+" ]";
+    public String toString() {
+        return "Employee [name: " + name + ", dept: " + dept + ", salary: " + salary + " ]";
     }
 }
--- src/test/java/structural/composite/CompositeTest.java
+++ src/test/java/structural/composite/CompositeTest.java
@@ -1,23 +1,45 @@
 package structural.composite;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
 import org.junit.Test;
 
+/**
+ * Composite pattern shows that objects can be composed together. Here we see an
+ * employee hierarchy.
+ */
 public class CompositeTest {
-    
+
     @Test
-    public void canAddEmployeeToEmployee(){
+    public void canAddEmployeeToEmployee() {
         Employee e1 = new Employee("Test", "HR", 5);
         Employee e2 = new Employee("Test 2", "Asst", 2.5);
-        
+
         e1.addSubordinate(e2);
-        
+
         List<Employee> empList = e1.getSubordinates();
-        
+
         assertTrue(empList.contains(e2));
+        e1.printSubordinates(); // Will show the hierarchy
+    }
+
+    @Test
+    public void canRemoveEmployeeFromEmployee() {
+        Employee e1 = new Employee("Test", "HR", 5);
+        Employee e2 = new Employee("Test 2", "Asst", 2.5);
+
+        e1.addSubordinate(e2);
+
+        List<Employee> empList = e1.getSubordinates();
+
+        assertTrue(empList.contains(e2));
+
+        e1.removeSubordinate(e2);
+
+        assertFalse(empList.contains(e2));
     }
 
 }
