04ab6b3f4a7eef41758064202213e2dd89fcc9d0
Neil Grogan <neil.grogan@ericsson.com>
2019-10-14 12:31:55 +0100

    Updated BuilderTest

--- src/main/java/creational/builder/House.java
+++ src/main/java/creational/builder/House.java
@@ -4,14 +4,26 @@ public class House {
     private HouseType houseType;
     private int noOfWindows;
     private int noOfDoors;
-    
-    House(final HouseType houseType, final int noOfWindows, final int noOfDoors){
+
+    House(final HouseType houseType, final int noOfWindows, final int noOfDoors) {
         this.houseType = houseType;
         this.noOfWindows = noOfWindows;
         this.noOfDoors = noOfDoors;
     }
-    
-    public String toString(){
+
+    public String toString() {
         return " HouseType: " + houseType + "\n No of Windows: " + noOfWindows + "\n No of Doors: " + noOfDoors;
+    }
+
+    public HouseType getHouseType() {
+        return this.houseType;
+    }
+
+    public int getNoOfWindows() {
+        return this.noOfWindows;
+    }
+
+    public int getNoOfDoors() {
+        return this.noOfDoors;
     }
 }
--- src/test/java/creational/builder/BuilderTest.java
+++ src/test/java/creational/builder/BuilderTest.java
@@ -1,38 +1,32 @@
 package creational.builder;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
+/**
+ * Similar to Factory (it builds objects for us), but we can build our objects
+ * incrementally.
+ * 
+ * We set each property we want, and when we are ready we can return the
+ * finished object.
+ */
 public class BuilderTest {
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
-    
-    @Before
-    public void setUp() throws Exception {
-        System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
-    }
-
-    @Ignore
     @Test
-    public void clone_and_change_prototype_properties() throws CloneNotSupportedException{
-    	
-    	HouseBuilder hb = new HouseBuilder(); // create a new builder
+    public void build_a_house_specifying_what_we_need() {
+
+        HouseBuilder hb = new HouseBuilder(); // create a new builder
         hb.setHouseType(HouseType.DETACHED); // set attribute on builder
         hb.setNoOfDoors(3); // set attribute on builder
-        hb.setNoOfWindows(3); // set attribute on builder
-        House house = hb.getResult(); // only now do we create a house when all the pieces are in place
-        System.out.println(house);
-         
-         // TODO - Validate 
+        hb.setNoOfWindows(7); // set attribute on builder
+        House h = hb.getResult(); // only now do we create a house when all the pieces are in place
+
+        assertTrue(h instanceof House);
+        assertEquals(HouseType.DETACHED, h.getHouseType());
+        assertEquals(3, h.getNoOfDoors());
+        assertEquals(7, h.getNoOfWindows());
     }
 
 }
