ce108888a0e6f2670485567375e53e5056f6c5bf
Neil Grogan <neil.grogan@ericsson.com>
2019-10-14 13:40:50 +0100

    Updated CommandPattern Test

--- src/main/java/behavioural/command/LightBulb.java
+++ src/main/java/behavioural/command/LightBulb.java
@@ -3,24 +3,31 @@ package behavioural.command;
 /**
  * @author Neil Grogan
  *
- * Standard Light Bulb with on and off switch.
+ *         Standard Light Bulb with on and off switch.
  *
  */
 public class LightBulb implements ILight {
+    private boolean lightIsOn = false;
 
     /**
-     *  Turns on the Light Bulb.
+     * Turns on the Light Bulb.
      */
     public void turnOn() {
-        System.out.println("Light is now on");
-
+        this.lightIsOn = true;
     }
 
     /**
      * Turns off the Light Bulb.
      */
     public void turnOff() {
-        System.out.println("Light is now off");
+        this.lightIsOn = false;
+    }
+
+    /**
+     * Gets the status of the light bulb
+     */
+    public boolean isLightOn() {
+        return this.lightIsOn;
     }
 
 }
--- src/test/java/behavioural/command/CommandPatternTest.java
+++ src/test/java/behavioural/command/CommandPatternTest.java
@@ -1,16 +1,64 @@
 package behavioural.command;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Command Pattern groups "commands" (anything the implements ICommand) and
+ * gives you the ability to execute them all or rollback with one method call.
+ * 
+ * Here we see we can wrap a light bulb class with commands, and then
+ * selectively run or undo them as we wish.
+ * 
+ */
 public class CommandPatternTest {
 
     @Test
-    @Ignore
-    public void test() {
-        fail("Not yet implemented");
+    public void light_switched_on_and_undo_with_command() {
+        LightBulb light = new LightBulb(); // Object we want to manipulate
+
+        TurnOnLightCommand tonlc = new TurnOnLightCommand(light); // command we create to wrap object
+
+        Switch sw = new Switch(); // command executor
+
+        sw.run(tonlc);
+        assertTrue(light.isLightOn());
+        sw.undo(tonlc);
+        assertFalse(light.isLightOn());
+    }
+
+    @Test
+    public void light_switched_off_and_undo_with_command() {
+        LightBulb light = new LightBulb(); // Object we want to manipulate
+
+        TurnOffLightCommand tofflc = new TurnOffLightCommand(light); // command
+
+        Switch sw = new Switch(); // command executor
+
+        sw.run(tofflc);
+        assertFalse(light.isLightOn());
+        sw.undo(tofflc);
+        assertTrue(light.isLightOn());
+    }
+
+    @Test
+    public void test2() {
+        LightBulb light = new LightBulb(); // Object we want to manipulate
+
+        TurnOffLightCommand tofflc = new TurnOffLightCommand(light); // command
+        TurnOnLightCommand tonlc = new TurnOnLightCommand(light); // command
+
+        Switch sw = new Switch(); // command executor
+
+        sw.run(tofflc);
+        sw.run(tonlc);
+
+        sw.undoAll();
+
+        assertTrue(light.isLightOn());
     }
 
 }
