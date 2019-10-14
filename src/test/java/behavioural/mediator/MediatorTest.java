313eb188833370e7cdc083b0e1851bf654b28b3e
Neil Grogan <neil.grogan@ericsson.com>
2019-10-14 12:39:12 +0100

    Fixed Date being off in Mediator Test

--- src/test/java/behavioural/mediator/MediatorTest.java
+++ src/test/java/behavioural/mediator/MediatorTest.java
@@ -10,38 +10,37 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MediatorTest {
-        final String john_username = "John";
-        final String roberts_username = "Robert";
-        final String johnsMessage = "Hi! Robert!";
-        final String robertsMessage = "Hello! John!";
-        private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    final String john_username = "John";
+    final String roberts_username = "Robert";
+    final String johnsMessage = "Hi! Robert!";
+    final String robertsMessage = "Hello! John!";
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
-        @Before
-        public void setUp() throws Exception {
-            System.setOut(new PrintStream(outContent));
-            System.setErr(new PrintStream(errContent));
-        }
-        
-        @Test
-        public void testChatMessagesSent(){
-            User robert = new User("Robert");
-            User john = new User("John");
+    @Before
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
 
-            robert.sendMessage(robertsMessage);
-            john.sendMessage(johnsMessage);
-            
-            final String johnChatLine = messageFormatter(john_username, johnsMessage);
-            assertTrue(outContent.toString().contains(johnChatLine));
-            
-            final String robertChatLine = messageFormatter(roberts_username, robertsMessage);
-            assertTrue(outContent.toString().contains(robertChatLine));
+    @Test
+    public void testChatMessagesSent() {
+        User robert = new User("Robert");
+        User john = new User("John");
 
-        }
+        robert.sendMessage(robertsMessage);
+        john.sendMessage(johnsMessage);
 
+        final String johnChatLine = messageFormatter(john_username, johnsMessage);
+        assertTrue(outContent.toString().contains(johnsMessage));
 
-        private String messageFormatter(final String username, final String message){
-            return new Date().toString() + " [" + username + "] : " + message;
-        }
+        final String robertChatLine = messageFormatter(roberts_username, robertsMessage);
+        assertTrue(outContent.toString().contains(robertsMessage));
+
+    }
+
+    private String messageFormatter(final String username, final String message) {
+        return new Date().toString() + " [" + username + "] : " + message;
+    }
 
 }
