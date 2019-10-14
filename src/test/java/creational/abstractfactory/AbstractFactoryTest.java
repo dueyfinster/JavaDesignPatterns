cfb73fdfeddc00870d7e1d433bf8960d9bbfd6d8
Neil Grogan <neil.grogan@ericsson.com>
2019-10-14 18:22:15 +0100

    Added proper AbstractFactoryTest

--- src/test/java/creational/abstractfactory/AbstractFactoryTest.java
+++ src/test/java/creational/abstractfactory/AbstractFactoryTest.java
@@ -3,34 +3,56 @@ package creational.abstractfactory;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
+/**
+ * Abstract Factories will do the heavy lifting (through other factories) of
+ * creating objects we need in a certain way.
+ * 
+ * For example, I want all of my Houses to be type "House" but some are in the
+ * form of a Bungalow, Semi-Detached etc etc. It's also abstracted to me that
+ * the houses are in the suburbs.
+ * 
+ */
 public class AbstractFactoryTest {
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
-    
-    @Before
-    public void setUp() throws Exception {
-        System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
-    }
-
-    @Ignore
     @Test
-    public void clone_and_change_prototype_properties() throws CloneNotSupportedException{
-    	
-    	 System.out.println(HouseFactory.buildHouse(HouseType.DETACHED) + "\n");
-         System.out.println(HouseFactory.buildHouse(HouseType.SEMI) + "\n");
-         System.out.println(HouseFactory.buildHouse(HouseType.BUNGALOW) + "\n");
-         System.out.println(HouseFactory.buildHouse(HouseType.COTTAGE));
-         
-         // TODO - Validate 
+    public void factory_will_build_detached_house() {
+
+        House h = HouseFactory.buildHouse(HouseType.DETACHED);
+
+        assertTrue(h instanceof House);
+        assertTrue(h instanceof DetachedHouse);
+        assertFalse(h instanceof SemiDetachedHouse);
     }
 
+    @Test
+    public void factory_will_build_semid_house() {
+
+        House h = HouseFactory.buildHouse(HouseType.SEMI);
+
+        assertTrue(h instanceof House);
+        assertTrue(h instanceof SemiDetachedHouse);
+        assertFalse(h instanceof DetachedHouse);
+    }
+
+    @Test
+    public void factory_will_build_bungalow_house() {
+
+        House h = HouseFactory.buildHouse(HouseType.BUNGALOW);
+
+        assertTrue(h instanceof House);
+        assertTrue(h instanceof BungalowHouse);
+        assertFalse(h instanceof CottageHouse);
+    }
+
+    @Test
+    public void factory_will_build_cottage_house() {
+
+        House h = HouseFactory.buildHouse(HouseType.COTTAGE);
+
+        assertTrue(h instanceof House);
+        assertTrue(h instanceof CottageHouse);
+        assertFalse(h instanceof BungalowHouse);
+    }
 }
